abdec845e79ab1e806c7f4ec509a9a7ec8427e3f
syt37du <syt37du@gmail.com>
2015-11-07 15:02:22 +0800

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,20 @@
+/**
+ * Created by 37DU on 2015/11/4.
+ */
+
+/*$(document).ready(function(){
+    var $nav_a=$('nav ul a');
+    for(var i=0;i<$nav_a.length;++i)
+    {
+        $nav_a.eq(i).width()+32;
+       // $nav_a.eq(i).css("backgroundImage","url(../images/nav_cur_bg.gif)");
+    }
+});*/
+
+
+window.onload=initi;
+
+function initi()
+{
+    //var
+}
